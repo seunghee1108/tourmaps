41ee064aa7ff4e4f706a38299ac74aa00ca46acb
bangseunghee <bangseunghee1108@gmail.com>
2024-01-31 11:40:43 +0900

    메인페이지 검색창에 검색어 입력하면 메인페이지에 뜸

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-"use client";
-import React, { useState } from "react";
+'use client'
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import styles from "@/app/styles/index.module.scss";
+import Topbar from "@/app/components/Topbar/Topbar";
+
+function Index() {
   const [searchKeyword, setSearchKeyword] = useState("");
   const [searchResult, setSearchResult] = useState("");
+  const [hasWindow, setHasWindow] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, [hasWindow]);
 
   const serviceKey =
     "WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D";
@@ -11,7 +22,7 @@ export default function Home() {
   const search = async () => {
     try {
       const response = await fetch(
-        `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?MobileOS=ETC&MobileApp=%EC%95%84%EC%95%84&_type=json&keyword=${searchKeyword}&serviceKey=WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D`
+        `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?MobileOS=ETC&MobileApp=%EC%95%84%EC%95%84&_type=json&keyword=${searchKeyword}&serviceKey=${serviceKey}`
       );
       if (response.ok) {
         const result = await response.json();
@@ -29,15 +40,28 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchKeyword}
-        onChange={handleInputChange} // 검색어 변경 핸들러 연결
-        placeholder="검색어를 입력하세요"
-      />
-      <button onClick={search}>검색</button>
-      <div id="result">{searchResult}</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Topbar />
+      </div>
+      <div className={`${styles.div} ${styles.gray}`}>
+        <div className={styles.box1}></div>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={searchKeyword}
+          onChange={handleInputChange} // 검색어 변경 핸들러 연결
+          placeholder="검색어를 입력하세요"
+        />
+        <button onClick={search}>검색</button>
+        <div id="result">{searchResult}</div>
+      </div>
+      <div className={`${styles.div} ${styles.yellow}`}></div>
+      <div className={`${styles.div} ${styles.purple}`}></div>
+      <div className={`${styles.div} ${styles.blue}`}></div>
     </div>
   );
 }
+
+export default Index;
