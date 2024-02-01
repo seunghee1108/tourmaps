4b29bd74888b60dd80adc510b0cb8d71fd95d33b
bangseunghee <bangseunghee1108@gmail.com>
2024-02-01 15:42:49 +0900

    서버코드 searchRegion 추가

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
+const cors = require("cors"); 
 
+app.use(cors());
 // API 키
 const serviceKey =
   "WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D";
@@ -15,7 +17,25 @@ app.get("/search", async (req, res) => {
     const { keyword } = req.query;
 
     // 검색을 위한 URL 생성
-    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?MobileOS=ETC&MobileApp=%EC%95%84%EC%95%84&_type=json&keyword=${keyword}&serviceKey=P1KIeVsld6hivJRj4DBnCnNrNwQJIDQZ6rk97%2FAK2g3U1Y1zrMMi0cRtRq%2BuvWT0vPN8J19qIDIs6FQxZPyhNA%3D%3D`;
+    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?numOfRows=100&MobileOS=ETC&MobileApp=%EC%95%84%EC%95%84&_type=json&keyword=${keyword}&serviceKey=${serviceKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+
+    // 서버에서 받은 데이터를 클라이언트에게 그대로 전달
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/searchRegion", async (req, res) => {
+  try {
+    const { keyword } = req.query;
+
+    // 지역 검색을 위한 URL 생성
+    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/areaCode?numOfRows=100&MobileOS=ETC&MobileApp=TUORMAPS&_type=json&serviceKey=${serviceKey}`;
 
     const response = await fetch(apiUrl);
     const data = await response.json();
