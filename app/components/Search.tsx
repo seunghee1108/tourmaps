6e6f571b019d753c237291466f2cb9178b597aef
bangseunghee <bangseunghee1108@gmail.com>
2024-02-01 09:46:57 +0900

    components-search 주석 처리

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,70 +1,36 @@
-// SearchComponent.tsx
+// "use client"
 
-// import React, { useState } from "react";
+// import React, { useState, FormEvent } from 'react';
+// import { useRouter } from "next/navigation";
+// import styles from "@/app/styles/search.module.scss";
 
 // interface SearchProps {
-//   onSearch: (keyword: string) => void;
+//   onSearchComplete: (query: string) => void;
 // }
 
-// const SearchComponent: React.FC<SearchProps> = ({ onSearch }) => {
-//   const [searchKeyword, setSearchKeyword] = useState("");
+// const Search: React.FC<SearchProps> = ({ onSearchComplete }) => {
+//   const [query, setQuery] = useState<string>('');
+//   const router = useRouter();
 
-//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setSearchKeyword(e.target.value);
-//   };
-
-//   const search = () => {
-//     onSearch(searchKeyword);
+//   const handleSearch = async (e: FormEvent) => {
+//     e.preventDefault();
+//     onSearchComplete(query); // 검색 완료 시 부모 컴포넌트로 검색어 전달
+//     router.push(`/search?q=${encodeURIComponent(query)}`);
 //   };
 
 //   return (
-
-    // <div>
-    //   <input
-    //     type="text"
-    //     value={searchKeyword}
-    //     onChange={handleInputChange}
-    //     placeholder="검색어를 입력하세요"
-    //   />
-    //   <button onClick={search}>검색</button>
-    // </div>
+//     <form onSubmit={handleSearch} className={styles.form}>
+//       <label htmlFor="searchInput">🔍</label>
+//       <input
+//         type="text"
+//         id="searchInput"
+//         placeholder="듣고싶은 곡을 검색하세요"
+//         className={styles.search}
+//         value={query}
+//         onChange={(e) => setQuery(e.target.value)}
+//       />
+//     </form>
 //   );
 // };
 
-// export default SearchComponent;
-"use client"
-
-import React, { useState, FormEvent } from 'react';
-import { useRouter } from "next/navigation";
-import styles from "@/app/styles/search.module.scss";
-
-interface SearchProps {
-  onSearchComplete: (query: string) => void;
-}
-
-const Search: React.FC<SearchProps> = ({ onSearchComplete }) => {
-  const [query, setQuery] = useState<string>('');
-  const router = useRouter();
-
-  const handleSearch = async (e: FormEvent) => {
-    e.preventDefault();
-    onSearchComplete(query); // 검색 완료 시 부모 컴포넌트로 검색어 전달
-    router.push(`/search?q=${encodeURIComponent(query)}`);
-  };
-
-  return (
-    <form onSubmit={handleSearch} className={styles.form}>
-      <label htmlFor="searchInput">🔍</label>
-      <input
-        type="text"
-        id="searchInput"
-        placeholder="가고싶은 여행지를 검색하세요"
-        className={styles.search}
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-    </form>
-  );
-};
-
-export default Search;
+// export default Search;
