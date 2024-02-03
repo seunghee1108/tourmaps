591215911690c255356065f3afbff338b6f10333
bangseunghee <bangseunghee1108@gmail.com>
2024-02-03 12:34:06 +0900

    에러 확인/ json 에러

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
-const cors = require("cors"); 
-
-app.use(cors());
 
 // API 키
 const serviceKey =
-  "WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4Lj1szQ%3D%3D";
+  "WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D";
 
 // 검색 결과를 받아올 엔드포인트
 const searchEndpoint =
@@ -18,7 +15,8 @@ app.get("/search", async (req, res) => {
     const { keyword } = req.query;
 
     // 검색을 위한 URL 생성
-    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?numOfRows=100&MobileOS=ETC&MobileApp=%EC%95%84%EC%95%84&_type=json&keyword=${keyword}&serviceKey=${serviceKey}`;
+    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?MobileOS=ETC&MobileApp=%EC%95%84%EC%95%84&_type=json&keyword=${keyword}&serviceKey=P1KIeVsld6hivJRj4DBnCnNrNwQJIDQZ6rk97%2FAK2g3U1Y1zrMMi0cRtRq%2BuvWT0vPN8J19qIDIs6FQxZPyhNA%3D%3D`;
+
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
@@ -31,18 +29,17 @@ app.get("/search", async (req, res) => {
   }
 });
 
-app.get("/searchRegion", async (req, res) => {
+app.get("/region", async (req, res) => {
   try {
-    const { keyword } = req.query;
+    const { selectedRegion } = req.query; // 클라이언트에서 전달하는 파라미터명을 selectedRegion으로 변경
 
-    // 지역 검색을 위한 URL 생성
-    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/areaCode?numOfRows=100&MobileOS=ETC&MobileApp=TUORMAPS&_type=json&serviceKey=${serviceKey}`;
+    // 검색을 위한 URL 생성
+    const apiUrl = `https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=${serviceKey}&numOfRows=10&MobileOS=ETC&MobileApp=Test&_type=json&areaCode=${selectedRegion}&contentTypeId=25`;
 
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
 
-    // 서버에서 받은 데이터를 클라이언트에게 그대로 전달
     res.json(data);
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -50,7 +47,7 @@ app.get("/searchRegion", async (req, res) => {
   }
 });
 
-app.get("/searchCourse", async (req, res) => {
+app.get("/course", async (req, res) => {
   try {
     // 코스 검색을 위한 URL 생성
     const apiUrl = `https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=${serviceKey}&numOfRows=10&MobileApp=AppTest&MobileOS=ETC&arrange=Q&areaCode=${currentRegion}&contentTypeId=25&cat1=C01&cat2=${currentHashtag}&_type=json`;
@@ -66,6 +63,7 @@ app.get("/searchCourse", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
