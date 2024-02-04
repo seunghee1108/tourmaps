0d4ebeaa1224b8e6254f71bb8897b01c60e058e1
bangseunghee <bangseunghee1108@gmail.com>
2024-02-04 23:17:58 +0900

    에러 해결해야됨 ㅜ

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cors = require("cors"); // CORS 미들웨어 추가
 const app = express();
 
 // API 키
@@ -9,6 +10,9 @@ const serviceKey =
 // 검색 결과를 받아올 엔드포인트
 const searchEndpoint =
   "http://apis.data.go.kr/B551011/KorService/searchKeyword";
+
+// CORS 미들웨어를 모든 요청에 적용
+app.use(cors());
 
 app.get("/search", async (req, res) => {
   try {
@@ -64,7 +68,6 @@ app.get("/course", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
