e7718c5471c89dddf562951dfc459d5de690c253
bangseunghee <bangseunghee1108@gmail.com>
2024-02-09 15:18:20 +0900

    디테일 페이지 테스트확인

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from "react";
+import styles from "@/app/styles/search.module.scss";
+import Topbar from "../components/Topbar/Topbar";
 
 const DetailPage: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <div>
-      <h1>Detail Page</h1>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Topbar />
+      </div>
+      <div>
+        <h1>Detail Page</h1>
+      </div>
     </div>
   );
 };
