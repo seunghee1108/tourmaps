985512959c786afc60e160c15b2ea444b5eae613
bangseunghee <bangseunghee1108@gmail.com>
2024-02-15 11:40:38 +0900

    서버 코드 수정

--- server.js
+++ server.js
@@ -6,53 +6,18 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// API 키
-const serviceKey =
-  "WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D";
-
 // 검색 결과를 받아올 엔드포인트
-const searchEndpoint =
-  "http://apis.data.go.kr/B551011/KorService/searchKeyword";
+// const searchEndpoint =
+//   "http://apis.data.go.kr/B551011/KorService/searchKeyword";
 
 app.prepare().then(() => {
   const server = express();
 
-  // server.get('/detail/:contentId', async (req, res) => {
-  //   const { contentId } = req.params; // 요청에서 contentId를 추출합니다.
-
-  //   try {
-  //     const commonInfoResponse = await fetch(
-  //       `http://apis.data.go.kr/B551011/KorService1/detailCommon1?ServiceKey=WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D&contentTypeId=25&contentId=${contentId}&MobileOS=ETC&MobileApp=AppTest&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&_type=json`
-  //       );
-  //       const introInfoResponse = await fetch(
-  //         `http://apis.data.go.kr/B551011/KorService1/detailIntro1?ServiceKey=WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D&contentTypeId=25&contentId=${contentId}&MobileOS=ETC&MobileApp=AppTest&_type=json`
-  //         );
-  //         const courseInfoResponse = await fetch(
-  //           `http://apis.data.go.kr/B551011/KorService1/detailInfo1?ServiceKey=WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D&contentTypeId=25&contentId=${contentId}&MobileOS=ETC&MobileApp=AppTest&_type=json`
-  //           );
-
-  //           if (commonInfoResponse.ok && introInfoResponse.ok && courseInfoResponse.ok) {
-  //             const commonInfo = await commonInfoResponse.json();
-  //             const introInfo = await introInfoResponse.json();
-  //             const courseInfo = await courseInfoResponse.json();
-
-  //             res.json({ commonInfo, introInfo, courseInfo });
-  //           } else {
-  //             res.status(500).json({ error: 'Failed to fetch data' });
-  //           }
-  //         } catch (error) {
-  //           console.error('Error fetching data:', error);
-  //           res.status(500).json({ error: 'Internal Server Error' });
-  //         }
-  //       });
-
   server.get("/detail/:contentId", (req, res) => {
     const contentId = req.params.contentId; // 요청에서 contentId를 추출합니다.
-      console.log(contentId);
+    console.log(contentId);
     return app.render(req, res, "/detail", { contentId });
   });
-  
-
 
   server.get("*", (req, res) => {
     return handle(req, res);
