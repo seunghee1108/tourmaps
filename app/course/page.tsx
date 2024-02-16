ea5f5f9e8f8f4206b107cd7f902793af47a8a252
bangseunghee <bangseunghee1108@gmail.com>
2024-02-16 16:00:18 +0900

    코스별페이지 수정

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -1,12 +1,9 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
+
 import React, { useState, useEffect } from "react";
+import "/app/globals.css";
 import styles from "@/app/styles/course.module.scss";
 import Topbar from "../components/Topbar/Topbar";
-
-interface SearchResultRegion {
-  name: string;
-}
 
 interface SearchResultList {
   addr1: string;
@@ -24,16 +21,14 @@ const SearchCoursePage = () => {
   const [currentRegion, setCurrentRegion] = useState<string>("");
   const [currentHashtag, setCurrentHashtag] = useState<string>("");
 
-  // const [params] = useSearchParams();
-
   useEffect(() => {
     fetchContent();
-  }, [currentRegion, currentHashtag ]);
+  }, [currentRegion, currentHashtag]);
 
   const fetchContent = async () => {
     try {
       const response = await fetch(
-        `https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D&numOfRows=10&MobileApp=AppTest&MobileOS=ETC&arrange=Q&areaCode=${currentRegion}&contentTypeId=25&cat1=C01&cat2=${currentHashtag}&_type=json`
+        `https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=WRM%2FxwABX2ibu1FMzeh0M4ca55og%2BubZJmgviYSiIEluTOFZkIWMZ3%2BqvAcSS85SpKyryvYtYgt1AX4JLj1szQ%3D%3D&numOfRows=20&MobileApp=AppTest&MobileOS=ETC&arrange=Q&areaCode=${currentRegion}&contentTypeId=25&cat1=C01&cat2=${currentHashtag}&_type=json`
       );
       if (response.ok) {
         const data = await response.json();
@@ -53,7 +48,7 @@ const SearchCoursePage = () => {
 
   const handleItemClick = (contentId: string) => {
     // contentId를 전달하고 DetailPage로 이동
-    window.location.href = `/detail/?contentId=${contentId}`; // 경로 수정 필요
+    window.location.href = `/detail/?contentId=${contentId}`;
   };
 
   return (
@@ -67,31 +62,37 @@ const SearchCoursePage = () => {
           alt="가족코스"
           onClick={() => handleHashtagClick("C0112")}
         />
+        {/* <p>#가족코스</p> */}
         <img
-          src="/travel.png"
+          src="/man.png"
           alt="나홀로코스"
           onClick={() => handleHashtagClick("C0113")}
         />
+        {/* <p>#나홀로코스</p> */}
         <img
           src="/healing.png"
           alt="힐링코스"
           onClick={() => handleHashtagClick("C0114")}
         />
+        {/* <p>#힐링코스</p> */}
         <img
-          src="/walk.png"
+          src="/shoes.png"
           alt="도보코스"
           onClick={() => handleHashtagClick("C0115")}
         />
+        {/* <p>#도보코스</p> */}
         <img
           src="/camping.png"
           alt="캠핑코스"
           onClick={() => handleHashtagClick("C0116")}
         />
+        {/* <p>#캠핑코스</p> */}
         <img
           src="/food.png"
           alt="맛코스"
           onClick={() => handleHashtagClick("C0117")}
         />
+        {/* <p>#맛코스</p> */}
       </div>
 
       <div className={styles.resultContainer}>
@@ -101,17 +102,19 @@ const SearchCoursePage = () => {
             <thead>
               <tr>
                 <th>TITLE</th>
-                <th>ADDRESS</th>
+                {/* <th>ADDRESS</th> */}
               </tr>
             </thead>
             <tbody>
               {searchResult.map((item, index) => (
-                <tr key={index} className={styles.row} onClick={() => handleItemClick(item.contentid)}>
-                  <td>
-                    
-                      {item.title}
-                  </td>
-                  <td>{item.contentid}</td>
+                <tr
+                  key={index}
+                  className={styles.row}
+                  onClick={() => handleItemClick(item.contentid)}
+                >
+                  <td>{item.title}</td>
+                  {/* <td>{item.addr1}</td> */}
+                  {/* <td>{item.contentid}</td> */}
                 </tr>
               ))}
             </tbody>
