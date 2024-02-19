a18ba71536440ee12358b450fd0a73308bff78dc
bangseunghee <bangseunghee1108@gmail.com>
2024-02-19 10:55:52 +0900

    topbar 필요없는 내용 삭제

--- app/components/Topbar/Topbar.tsx
+++ app/components/Topbar/Topbar.tsx
@@ -1,37 +1,10 @@
 // import jwt, { JwtPayload } from 'jsonwebtoken'; // Commented out this import
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "@/app/styles/topbar.module.scss"
 import Link from "next/link";
 import TopNav from "./topnav";
 
 function Topbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [name, setName] = useState<string>(''); 
-
-  useEffect(() => {
-    const loadUserFromToken = () => {
-      const token = localStorage.getItem('token');
-
-      if (token) {
-        const userName = token; 
-        setIsLoggedIn(true);
-        setName(userName);
-      } else {
-        setIsLoggedIn(false);
-        setName('');
-      }
-    };
-
-    loadUserFromToken();
-  }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setIsLoggedIn(false);
-    setName('');
-    window.location.href = '/';
-  };
-
   return (
     <div className={styles.topbar}>
       <div className={styles.div1}><TopNav/></div>
@@ -41,7 +14,6 @@ function Topbar() {
       <div className={styles.div3}>
       <Link className={styles.link} href="/region">REGION</Link>
       <Link className={styles.link} href="/course">COURSE</Link>
-
       </div>
     </div>
   );
