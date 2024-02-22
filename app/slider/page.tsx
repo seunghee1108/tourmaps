be3105eacb2ee9d66dea996d606fa28371c1870d
bangseunghee <bangseunghee1108@gmail.com>
2024-02-22 16:51:12 +0900

    슬라이드 페이지 주석 삭제

--- app/slider/page.tsx
+++ app/slider/page.tsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
-import styles from '../styles/slide.module.scss';
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from "react";
+import styles from "../styles/slide.module.scss";
 import "/app/globals.css";
 
 const SlidePage: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const slides = [
-    { imageUrl: '/slide.jpg' },
-    // { imageUrl: '/slideimage.jpg' },
-    { imageUrl: '/slideimage2.jpg' }
-  ];
+  const slides = [{ imageUrl: "/slide.jpg" }, { imageUrl: "/slideimage2.jpg" }];
 
   const nextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+    setCurrentSlide((prevSlide) =>
+      prevSlide === slides.length - 1 ? 0 : prevSlide + 1
+    );
   };
 
   const prevSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? slides.length - 1 : prevSlide - 1
+    );
   };
 
   return (
@@ -23,7 +24,9 @@ const SlidePage: React.FC = () => {
       {slides.map((slide, index) => (
         <div
           key={index}
-          className={`${styles.slide} ${index === currentSlide ? styles.active : ''}`}
+          className={`${styles.slide} ${
+            index === currentSlide ? styles.active : ""
+          }`}
         >
           <img
             src={slide.imageUrl}
@@ -32,8 +35,12 @@ const SlidePage: React.FC = () => {
           />
         </div>
       ))}
-      <button className={styles.arrow} onClick={prevSlide}>{'<'}</button>
-      <button className={styles.arrow} onClick={nextSlide}>{'>'}</button>
+      <button className={styles.arrow} onClick={prevSlide}>
+        {"<"}
+      </button>
+      <button className={styles.arrow} onClick={nextSlide}>
+        {">"}
+      </button>
     </div>
   );
 };
