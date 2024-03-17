109084c443dd7332373308e45bbeb0b7edfd9e47
bangseunghee <bangseunghee1108@gmail.com>
2024-03-17 12:47:29 +0900

    내용 수정

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -11,10 +11,10 @@ function Footer() {
     <div>
       <p>이용약관 l 개인정보처리방침</p>
       <p>CEO : 그린</p>
-      <p>H.P : 070-3232-3232</p>
-      <p>FAX : +82-02-3232-3233</p>
+      <p>H.P : 012-3456-7890</p>
+      <p>FAX : +82-02-7777-7777</p>
       <p>ADDRESS : 대전광역시 서구 대덕대로</p>
-      <p>ⓒ 2023 MK. All rights reserved</p>
+      <p>ⓒ 2024 MK. All rights reserved</p>
     </div>
   </div>
   );
