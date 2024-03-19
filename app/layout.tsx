3c2011c7cfc5ff4df0da1277dda503baf3a94ca0
bangseunghee <bangseunghee1108@gmail.com>
2024-03-19 13:12:13 +0900

    title 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import styles from "@/app/styles/index.module.scss";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TOURMAPS",
+  description: "키워드를 통한 검색기능과 지역별 코스별로 여행 코스를 제공하는 웹 페이지입니다.",
+};
+
 
 export default function RootLayout({
   children,
